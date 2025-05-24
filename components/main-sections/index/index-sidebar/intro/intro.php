df19cd7a321117e39fe343146a93a00c32df1bff
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-05-24 09:03:17 -0600

    ..

--- components/main-sections/index/index-sidebar/intro/intro.php
+++ components/main-sections/index/index-sidebar/intro/intro.php
@@ -3,7 +3,7 @@
 	<header>
 		<h2>Love and Chaos Dot Space</h2>
 		<p>A Shaman's Guide to the Other Side.  Essays, articles, and technologies for overcoming the sickness 
-			and doubt of surviving PTSD, and learning to live with purpose and pride again.			
+			and doubt of surviving PTSD, and learning to live with pride and purpose again.			
 		</p>
 		<p>by: <span style="color: teal;">Maestro Drago Jax</span></p>
 	</header>
