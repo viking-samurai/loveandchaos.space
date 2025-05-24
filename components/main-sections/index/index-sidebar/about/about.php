2d414c81805fd682e9b93001c2a554c4f59f9f47
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-05-24 11:23:54 -0600

    ..

--- components/main-sections/index/index-sidebar/about/about.php
+++ components/main-sections/index/index-sidebar/about/about.php
@@ -1,11 +1,12 @@
 <section class="blurb">
 	<h2>About</h2>
-	<p>Maestro Drago Jax is practicing Shaman within Calgary and the surrounding area.  He specializes in PTSD recovery 
-		using Acid, Shrooms, Weed, and Whiskey.  He further specializes in the science of re-invigoration as a faith-based 
-		practice, based on the presumption of a Soul, and its necessity to healthy, productive living.
+	<p>Maestro Drago Jax is a Shaman operating within Calgary and the surrounding area.  He specializes 
+		in PTSD recovery using Acid, Shrooms, Weed, and Whiskey.  His operation focuses on the science 
+		of re-invigoration as a faith-based practice, based on the presumption of a Soul, and its importance 
+		to a healthy, productive life.
 	</p>
-	<p>If you or someone you know is suffering from PTSD, or serious mental illness of a debilitating nature, and would like 
-		to talk about using LSD or Psilocybin to treat such conditions, feel free to reach out.
+	<p>If you or someone you know is suffering from PTSD and would like to talk about using LSD and Psilocybin 
+		to treat this disorder, feel free to reach out.
 	</p>
 	<ul class="actions">
 		<li><a href="#" class="button">Learn More</a></li>
