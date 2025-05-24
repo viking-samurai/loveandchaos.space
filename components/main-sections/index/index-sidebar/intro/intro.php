007e1df2a711f48131c31cf2ecb68aeb95861afb
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-05-24 09:02:05 -0600

    ..

--- components/main-sections/index/index-sidebar/intro/intro.php
+++ components/main-sections/index/index-sidebar/intro/intro.php
@@ -2,8 +2,8 @@
 	<a href="#" class="logo"><img src="images/Rebecca-Guay/main_Banner.png" alt="" /></a>
 	<header>
 		<h2>Love and Chaos Dot Space</h2>
-		<p>A Shaman's Guide to the Other Side.  Essays, articles, and ideas on overcoming the sickness and doubt 
-			of surviving PTSD, and learning to live with purpose and pride again.			
+		<p>A Shaman's Guide to the Other Side.  Essays, articles, and technologies for overcoming the sickness 
+			and doubt of surviving PTSD, and learning to live with purpose and pride again.			
 		</p>
 		<p>by: <span style="color: teal;">Maestro Drago Jax</span></p>
 	</header>
